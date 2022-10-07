a3aa6ab56365c2ffa7385e042a7d6b8415539de7
Merto <mertenercan@gmail.com>
2022-10-07 08:31:09 +0300

    Final, i guess ?

--- app/src/pages/Home/Home.js
+++ app/src/pages/Home/Home.js
@@ -1,42 +1,87 @@
-import { Fragment, useContext } from "react";
+import { Fragment, useContext, useEffect } from "react";
 import MainCard from "../../components/MainCard/MainCard";
 import "./home.scss"
-import InfiniteScrolll from "react-infinite-scroller"
 import { NavLink } from "react-router-dom";
 import { ArtistContext } from "../../store/context";
 import Loading from "../../components/Loading/Loading";
-import { useTopArtists } from "../../hooks/useTopArtists";
-
+import {useInView} from "react-intersection-observer"
+import { useInfiniteQuery } from "react-query";
+import { isEqual, uniqWith } from "lodash";
+import axios from "axios";
 
 
 const Home = (props) => {
-    
+
+                        /* I'LL BE HONEST WITH YOU, FETCHING TOP ARTIST DATA CODES ARE NOT MINE I STOLE IT FROM @alperalin */ 
+                                
     const ctx = useContext(ArtistContext)
-
-    //Fetching data from custom React Query hook (Top Artists) and checking loading state
-
-    const {data:queryData, isLoading, isError, error, fetchNextPage, } = useTopArtists()
-
-    if(isLoading){
-        return <Loading />
+    
+    //Data Fetch fn
+    const fetchData = async (pageNumber) => {
+        return await axios(`https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&page=${pageNumber}&limit=10&api_key=${process.env.REACT_APP_API_KEY}&format=json`)
     }
-    if(isError){
-        return <h2>{error.message}</h2>
-    }
+
+    /* Infinite query */
+    const {data:queryData, fetchNextPage, isLoading, } = useInfiniteQuery(
+        'Top Artists',
+        ({ pageParam = 1 }) => fetchData(pageParam).then((res) => res.data),
+        {
+          select: (data) => ({
+            pages: [
+              ...uniqWith(
+                data.pages.map((item) => item['artists']['artist']).flat(),
+                isEqual
+              ),
+            ],
+          }),
+
+          getNextPageParam: (lastPage, pages) => {
+
+            let page = parseInt(lastPage.artists['@attr'].page);
+            const totalPage = parseInt(lastPage.artists['@attr'].totalPages);
+            if (page < totalPage) {
+
+                return ++page;
+            }
+
+            return undefined;
+
+          },
+        }
+      );
+       /* Infinite query */
+
+       
+      //Inview config
+      const { ref, inView } = useInView({
+        trackVisibility: true,
+        delay: 500,
+      });
+
+      //Fetch data with scroll
+      useEffect(() => {
+        if (inView) fetchNextPage();
+      }, [inView]);
+
+ 
+if(isLoading){
+    return(<Loading />)
+}
 
     return(
+
         <div data-testid='border' className="artists-wrapper">
 
                 <div className="artists-section" >
 
-                    <InfiniteScrolll useWindow={false} hasMore={true} loadMore={fetchNextPage}>
+                    
 
-                        {queryData?.pages.map((page,i) => 
+                        {queryData?.pages.map((artist,i) => 
 
-                            <Fragment key={i}>
+                            <Fragment  key={i}>
 
-                            {page.data.artists.artist.map(artist => 
-                            <NavLink key={artist.name} 
+                           
+                            <NavLink  ref={ref}key={artist.name} 
                             onClick={() => ctx.onCurrentArtist(artist)} 
                             className={'artist-card'} 
                             to={artist.mbid}> 
@@ -47,7 +92,7 @@ const Home = (props) => {
                                 playcount={artist.playcount} 
                                 listeners={artist.listeners}/>
 
-                            </NavLink>)}
+                            </NavLink>
                        
                       
                         </Fragment>
@@ -55,7 +100,7 @@ const Home = (props) => {
                         }
                     
 
-                    </InfiniteScrolll>
+          
 
                 </div>
             
